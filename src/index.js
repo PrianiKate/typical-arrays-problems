1573b899b03019de3eb3ee1631220d3cec88e368
PrianiKate <li.liliana@yandex.ru>
2021-01-23 14:00:09 +0400

    feat: solve problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (arguments.length == 0 || array.length == 0) return 0;
+    let ans = Math.min(array[0]);
+    for (let i = 1; i < array.length; i++)
+        ans = Math.min(ans, array[i]);
+    return ans;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (arguments.length == 0 || array.length == 0) return 0;
+    let ans = Math.max(array[0]);
+    for (let i = 1; i < array.length; i++)
+        ans = Math.max(ans, array[i]);
+    return ans;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (arguments.length == 0 || array.length == 0) return 0;
+    return array.reduce((acc, cur) => cur + acc) / array.length;
 }
